0466a9e4d6b6fb5b03b9ec0983488e4f3d06e9e8
Andrea Borruso <aborruso@pc-aborruso.ticonzero.locale>
2015-11-21 09:06:46 +0100

    definito schema dati

---
+++ schema.js
@@ -0,0 +1,18 @@
+{
+    "types": {
+        "President": {
+            "pluralLabel": "Proposte"
+        },
+        "Presidency": {
+            "pluralLabel": "Proposte"
+        }
+    },
+    "properties": {
+        "ID": {
+            "valueType": "number"
+        },
+        "label": {
+            "valueType":              "item"
+        }
+    }
+}
